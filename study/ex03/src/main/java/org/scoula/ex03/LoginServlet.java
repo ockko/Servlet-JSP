8db8205a7e84f1c594e0ae7da2912498ab803e27
ockko <ocko119@gmail.com>
2025-06-17 01:21:46 +0900

    [연습] 서블릿에서 파라미터 처리(GET 방식)

---
+++ study/ex03/src/main/java/org/scoula/ex03/LoginServlet.java
@@ -0,0 +1,27 @@
+package org.scoula.ex03;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userid = req.getParameter("userid");
+        String passwd = req.getParameter("passwd");
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("userid : " + userid + "<br />");
+        out.println("passwd : " + passwd);
+        out.println("</body></html>");
+    }
+}
