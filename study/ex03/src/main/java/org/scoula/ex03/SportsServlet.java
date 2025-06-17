ae5a40595f8dd6b4beadbd81ab8ae66a62cb172f
ockko <ocko119@gmail.com>
2025-06-17 22:56:18 +0900

    [연습] getParameterValues, getParameterNames

--- study/ex03/src/main/java/org/scoula/ex03/SportsServlet.java
+++ study/ex03/src/main/java/org/scoula/ex03/SportsServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/sports")
 public class SportsServlet extends HttpServlet {
@@ -13,6 +14,18 @@ public class SportsServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        
+
+        String[] sports = req.getParameterValues("sports");
+        String sex = req.getParameter("sex");
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        for (String sport : sports) {
+            out.println("좋아하는 운동: " + sport + "<br/>");
+        }
+        out.println("성별: " + sex);
+        out.println("</body></html>");
     }
 }
