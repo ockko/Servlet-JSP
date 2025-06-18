be8b10e709b097e67b0c9b49abd50854feaf33fd
ockko <ocko119@gmail.com>
2025-06-18 17:49:26 +0900

    [연습] CartViewServlet 추가

--- study/ex04/src/main/java/org/scoula/ex04/session/CartViewServlet.java
+++ study/ex04/src/main/java/org/scoula/ex04/session/CartViewServlet.java
@@ -10,31 +10,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-@WebServlet("/cart_save")
-public class CartSaveServlet extends HttpServlet {
+@WebServlet("/cart_view")
+public class CartViewServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
 
         PrintWriter out = resp.getWriter();
-
-        String product = req.getParameter("product");
-
-        HttpSession session = req.getSession();
-        ArrayList<String> list = (ArrayList<String>) session.getAttribute("product");
-
-        if (list == null) {
-            list = new ArrayList<String>();
-            session.setAttribute("product", list);
-        }
-
-        list.add(product);
-
         out.println("<html><body>");
-        out.println("Product 추가!!<br/>");
+        out.println("장바구니 리스트상품: ");
+
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            session.setMaxInactiveInterval(20);
+            ArrayList<String> list = (ArrayList<String>) session.getAttribute("product");
+            out.println(list + "<br/>");
+        } else {
+            out.println("세션 없음<br/>");
+        }
         out.println("<a href='session_product.jsp'>상품 선택 페이지</a><br/>");
-        out.println("<a href='cart_view'>장바구니 보기</a>");
+        out.println("<a href='cart_delete'>장바구니 비우기</a>");
         out.println("</body></html>");
     }
 }
